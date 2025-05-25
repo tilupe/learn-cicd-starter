d18c94cf3615885e1da9bfbeb1eb65b694430545
tilupe <timothy@pescatore.ch>
2025-05-25 16:15:51 +0200

    add new test

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,20 @@
+package auth
+
+import (
+	"reflect"
+	"testing"
+	"net/http"
+)
+
+func TestGetApiKey(t *testing.T) {
+	headers := make(http.Header)
+	headers.Add("Authorization", "ApiKey test_api_key")
+	got, error := GetAPIKey(headers)
+	want := "test_api_key"
+	if error != nil {
+		t.Fatalf("expected no error, got %v", error)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("expected = %v, got %v", want, got)
+	}
+}
